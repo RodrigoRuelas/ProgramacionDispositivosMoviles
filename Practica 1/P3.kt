080e884678dc215e2ca1b099ee507df1d2937167
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-24 16:54:22 -0500

    Problema 3

--- Practica 1/P3.kt
+++ Practica 1/P3.kt
@@ -1,11 +1,28 @@
 // Pregunta 3. Implementación de una calculadora básica.
 // De: Rodrigo Alonso Ruelas Lope
 // Fecha de Creacion: 24/08/2024 - 04:00 pm
-// Ultima modificacion: 24/08/2024 - 04:37 pm
+// Ultima modificacion: 24/08/2024 - 04:50 pm
 
 fun main() {
+    println("¡Bienvenido a la calculadora en Kotlin!")
     while (true) {
-        println("Bienvenido a la calculadora en Kotlin!")
+        val operacion : Int = seleccionarOperacion()
+
+        if (operacion == 5) {
+            println("Saliendo de la calculadora. ¡Adiós!")
+            break
+        }
+
+        val num1 = obtenerNumero("Ingresa el primer número:")
+        val num2 = obtenerNumero("Ingresa el segundo número:")
+
+        val resultado = realizarOperacion(operacion, num1, num2)
+        println("El resultado de la operación es: $resultado\n")
+    }
+}
+
+fun seleccionarOperacion(): Int {
+    while (true) {
         println("Selecciona una operación:")
         println("1. Suma")
         println("2. Resta")
@@ -15,42 +32,56 @@ fun main() {
 
         val operacion = readLine()?.toIntOrNull()
 
-        if (operacion == 5) {
-            println("Saliendo de la calculadora. ¡Adiós!")
-            break
-        }
-
-        if (operacion !in 1..4) {
+        if (operacion != null && operacion in 1..5) {
+            return operacion
+        } else {
             println("Opción inválida. Por favor selecciona una operación válida.")
-            continue
         }
-
-        println("Ingresa el primer número:")
-        val num1 = readLine()?.toDoubleOrNull()
-
-        println("Ingresa el segundo número:")
-        val num2 = readLine()?.toDoubleOrNull()
-
-        if (num1 == null || num2 == null) {
-            println("Entrada inválida. Por favor ingresa números válidos.")
-            continue
-        }
-
-        val resultado = when (operacion) {
-            1 -> num1 + num2
-            2 -> num1 - num2
-            3 -> num1 * num2
-            4 -> {
-                if (num2 == 0.0) {
-                    println("Error: No se puede dividir por cero.")
-                    continue
-                } else {
-                    num1 / num2
-                }
-            }
-            else -> 0.0
-        }
-
-        println("El resultado de la operación es: $resultado\n")
     }
+}
+
+
+fun obtenerNumero(mensaje: String): Double {
+    println(mensaje)
+    val numero = readLine()?.toDoubleOrNull()
+
+    if (numero == null) {
+        println("Entrada inválida. Por favor ingresa un número válido.")
+        return obtenerNumero(mensaje)
+    }
+
+    return numero
+}
+
+fun realizarOperacion(operacion: Int, num1: Double, num2: Double): Double {
+    return when (operacion) {
+        1 -> suma(num1, num2)
+        2 -> resta(num1, num2)
+        3 -> multiplicacion(num1, num2)
+        4 -> {
+            if (num2 == 0.0) {
+                println("Error: No se puede dividir por cero.")
+                0.0
+            } else {
+                division(num1, num2)
+            }
+        }
+        else -> 0.0
+    }
+}
+
+fun suma(a: Double, b: Double): Double {
+    return a + b
+}
+
+fun resta(a: Double, b: Double): Double {
+    return a - b
+}
+
+fun multiplicacion(a: Double, b: Double): Double {
+    return a * b
+}
+
+fun division(a: Double, b: Double): Double {
+    return a / b
 }
