8f9b0dd0b23cb2d032a17135339fa5c714971d25
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-25 23:11:30 -0500

    P4.kt

--- Practica 1/P4.kt
+++ Practica 1/P4.kt
@@ -5,7 +5,9 @@
 
 import kotlin.random.Random
 
+// Main
 fun main() {
+    // Variables donde se determina el rango del numero secreto y la cantidad de intentos
     val numeroSecreto = generarNumeroSecreto(1, 30)
     val intentosMaximos = 5
 
@@ -21,25 +23,30 @@ fun main() {
     }
 }
 
+// Funcion para generar el numero a adivinar
 fun generarNumeroSecreto(inicio: Int, fin: Int): Int {
     return Random.nextInt(inicio, fin + 1)
 }
 
+// Desarrollo del juego
 fun jugar(intentosMaximos: Int, numeroSecreto: Int): Boolean {
     var intentosRestantes = intentosMaximos
 
     while (intentosRestantes > 0) {
         val suposicion = obtenerSuposicion()
 
+        // Verificacion del numero ingresado
         if (suposicion == null) {
             println("Por favor, introduce un número válido.")
             continue
         }
 
+        // Evaluacion de la entrada
         if (evaluarSuposicion(suposicion, numeroSecreto)) {
             return true
         }
 
+        // Reduccion de intentos
         intentosRestantes--
         if (intentosRestantes > 0) {
             println("Te quedan $intentosRestantes intentos.")
@@ -48,13 +55,16 @@ fun jugar(intentosMaximos: Int, numeroSecreto: Int): Boolean {
     return false
 }
 
+// Funcion para ingresar el numero supuesto
 fun obtenerSuposicion(): Int? {
     println("\nIntroduce tu suposición:")
     return readLine()?.toIntOrNull()
 }
 
+// Funcion donde se verifica el numero ingresado y el numero secreto
 fun evaluarSuposicion(suposicion: Int, numeroSecreto: Int): Boolean {
     return when {
+        // Evaluacion de la entrada con pista
         suposicion < numeroSecreto -> {
             println("¡Demasiado bajo!")
             false
