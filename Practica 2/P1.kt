1ab35811fad3686d86c497a6e4d8c067354c988e
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 18:43:49 -0500

    P1. Creando archivo

---
+++ Practica 2/P1.kt
@@ -0,0 +1,8 @@
+// Pregunta 1: Creando una cuenta bancaria
+// De: Rodrigo Alonso Ruelas Lope
+// Fecha de Creacion: 31/08/2024 - 6:43 pm
+// Ultima modificacion: 31/08/2024 - 0:00 pm
+
+fun main() {
+  println("Hola mundo")
+}  
