4169f3afd41b56b9c584ab104eaed21bc9339799
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 18:45:39 -0500

    P1. Determinando la clase CuentaBancaria con sus atributos

--- Practica 2/P1.kt
+++ Practica 2/P1.kt
@@ -3,6 +3,10 @@
 // Fecha de Creacion: 31/08/2024 - 6:43 pm
 // Ultima modificacion: 31/08/2024 - 0:00 pm
 
+class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
+
+}  
+
 fun main() {
-  println("Hola mundo")
+  val cuenta = CuentaBancaria(1000.0, 500.0)
 }  
