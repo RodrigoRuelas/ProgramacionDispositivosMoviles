10562c775f83af8641ec021f113ade67b41a0534
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 20:02:39 -0500

    P2. Creando archivo

---
+++ Practica 2/P2.kt
@@ -0,0 +1,8 @@
+// Pregunta 2: Creando un producto con sus descuentos
+// De: Rodrigo Alonso Ruelas Lope
+// Fecha de Creacion: 31/08/2024 - 8:02 pm
+// Ultima modificacion: 31/08/2024 - 0:00 pm
+
+fun main() {
+  println("Hola mundo")
+}  
