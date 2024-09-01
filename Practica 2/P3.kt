2935f699c7fe60354f361c345822d74c16109111
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 20:29:01 -0500

    P3. Insertando la clase abstracta y las clases de las Figuras

--- Practica 2/P3.kt
+++ Practica 2/P3.kt
@@ -3,6 +3,26 @@
 // Fecha de Creacion: 31/08/2024 - 8:23 pm
 // Ultima modificacion: 31/08/2024 - 0:00 pm
 
+// Clase abstracta Shape
+abstract class Shape {
+
+}
+
+// Subclase Cuadrado
+class Cuadrado(private val lado: Double) : Shape() {
+
+}
+
+// Subclase Círculo
+class Circulo(private val radio: Double) : Shape() {
+
+}
+
+// Subclase Rectángulo
+class Rectangulo(private val largo: Double, private val ancho: Double) : Shape() {
+
+}
+
 fun main() {
   println("Hola mundo")
 }  
