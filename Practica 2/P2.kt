df2fd72c418c59f5d26c6fc61cc1eab81ff7dedf
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 20:09:47 -0500

    P2. Ingresando getters y setters en la clase Producto

--- Practica 2/P2.kt
+++ Practica 2/P2.kt
@@ -4,9 +4,35 @@
 // Ultima modificacion: 31/08/2024 - 0:00 pm
 
 class Producto(private var precio: Double, private var descuento: Double) {
+    // Método getter para obtener el precio
+    fun getPrecio(): Double {
+        return precio
+    }
 
+    // Método setter para establecer el precio, con validación para no permitir precios negativos
+    fun setPrecio(nuevoPrecio: Double) {
+        if (nuevoPrecio >= 0) {
+            precio = nuevoPrecio
+        } else {
+            println("El precio no puede ser negativo.")
+        }
+    }
+
+    // Método getter para obtener el descuento
+    fun getDescuento(): Double {
+        return descuento
+    }
+
+    // Método setter para establecer el descuento, con validación para no permitir descuento negativo o mayor al 100%
+    fun setDescuento(nuevoDescuento: Double) {
+        if (nuevoDescuento in 0.0..100.0) {
+            descuento = nuevoDescuento
+        } else {
+            println("El descuento debe estar entre 0% y 100%.")
+        }
+    }
 }  
 
 fun main() {
-  val producto = Producto(150.0, 10.0)
+    val producto = Producto(150.0, 10.0)
 }  
