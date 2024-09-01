04321d52ca0f03a8ded38ed7d07e3c9200951023
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-08-31 20:24:12 -0500

    P3. Creando archivo

---
+++ Practica 2/P3.kt
@@ -0,0 +1,8 @@
+// Pregunta 3: Creando un programa para determinar area y perimetro de figuras geometricas
+// De: Rodrigo Alonso Ruelas Lope
+// Fecha de Creacion: 31/08/2024 - 8:23 pm
+// Ultima modificacion: 31/08/2024 - 0:00 pm
+
+fun main() {
+  println("Hola mundo")
+}  
