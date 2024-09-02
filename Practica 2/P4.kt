40e8bf7b4a6fd952a906f2df2fea0ed02d3bb9c0
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-09-01 22:54:15 -0500

    P4. Ingresando las clases e interfaz requeridos

--- Practica 2/P4.kt
+++ Practica 2/P4.kt
@@ -3,3 +3,22 @@
 // Fecha de Creacion: 1/09/2024 - 10:50 pm
 // Ultima modificacion: 1/08/2024 - 0:00 pm
 
+// Clase abstracta Material
+abstract class Material()
+
+// Subclase Libro
+class Libro()
+
+// Subclase Revista
+class Revista()
+
+// Data class Usuario
+data class Usuario()
+
+// Interfaz IBiblioteca
+interface IBiblioteca {}
+
+// Clase Biblioteca que implementa IBiblioteca
+class Biblioteca : IBiblioteca {}
+
+fun main() {}
