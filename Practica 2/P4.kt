11b7a7090084376652d660b3ea2080624fe5a581
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-09-01 23:13:26 -0500

    P4. Ingresando datos en las clases y en la interfaz

--- Practica 2/P4.kt
+++ Practica 2/P4.kt
@@ -5,7 +5,7 @@
 
 // Clase abstracta Material
 abstract class Material(
-   val titulo: String,
+    val titulo: String,
     val autor: String,
     val anioPublicacion: Int
 ) {
@@ -13,16 +13,61 @@ abstract class Material(
 }
 
 // Subclase Libro
-class Libro()
+class Libro(
+    titulo: String,
+    autor: String,
+    anioPublicacion: Int,
+    val genero: String,
+    val numeroPaginas: Int
+) : Material(titulo, autor, anioPublicacion) {
+
+    override fun mostrarDetalles() {
+        println("Título: $titulo")
+        println("Autor: $autor")
+        println("Año de Publicación: $anioPublicacion")
+        println("Género: $genero")
+        println("Número de Páginas: $numeroPaginas")
+    }
+}
 
 // Subclase Revista
-class Revista()
+class Revista(
+    titulo: String,
+    autor: String,
+    anioPublicacion: Int,
+    val issn: String,
+    val volumen: Int,
+    val numero: Int,
+    val editorial: String
+) : Material(titulo, autor, anioPublicacion) {
+
+    override fun mostrarDetalles() {
+        println("Título: $titulo")
+        println("Autor: $autor")
+        println("Año de Publicación: $anioPublicacion")
+        println("ISSN: $issn")
+        println("Volumen: $volumen")
+        println("Número: $numero")
+        println("Editorial: $editorial")
+    }
+}
 
 // Data class Usuario
-data class Usuario()
+data class Usuario(
+    val nombre: String,
+    val apellido: String,
+    val edad: Int
+)
 
 // Interfaz IBiblioteca
-interface IBiblioteca {}
+interface IBiblioteca {
+    fun registrarMaterial(material: Material)
+    fun registrarUsuario(usuario: Usuario)
+    fun prestarMaterial(usuario: Usuario, material: Material): Boolean
+    fun devolverMaterial(usuario: Usuario, material: Material): Boolean
+    fun mostrarMaterialesDisponibles()
+    fun mostrarMaterialesReservadosPorUsuario(usuario: Usuario)
+}
 
 // Clase Biblioteca que implementa IBiblioteca
 class Biblioteca : IBiblioteca {}
