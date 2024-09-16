4e15188db84ecd414bdb73f0d62d5cd7b7fcfe02
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-09-15 22:14:48 -0500

    Implementado la clase e importando las herramientas necesarias

---
+++ Practica 3/Ejercicio 1/Main.kt
@@ -0,0 +1,10 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+}
