154af281d29e717341c77c2781c7eac9522ef94a
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-09-15 22:26:14 -0500

    Implementado la clase de la Imagen e importando las herramientas necesarias

---
+++ Practica 3/Ejercicio 1/Imagen.kt
@@ -0,0 +1,8 @@
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageActivity : AppCompatActivity() {
+
+}
