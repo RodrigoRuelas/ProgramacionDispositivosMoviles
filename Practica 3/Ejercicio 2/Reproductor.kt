3bf639e997c1a0cd8215d7ad13d3e23b84888364
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2024-09-15 23:05:34 -0500

    Creado la clase y las importaciones para reproducir audio

---
+++ Practica 3/Ejercicio 2/Reproductor.kt
@@ -0,0 +1,10 @@
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class AudioPlayerActivity : AppCompatActivity() {
+
+}
